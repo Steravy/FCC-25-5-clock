e7f758f9ed149b81b255840576e399d25171a680
Stefan Vitoria <seacod3@sapo.pt>
2023-07-24 19:08:32 -0100

    install dependencies

---
+++ src/Home.tsx
@@ -0,0 +1,22 @@
+import { Fragment } from 'react';
+import Navbar from './components/navbar/Navbar'
+import Container from './components/Container';
+
+function Home() {
+
+
+  return (
+
+    <Fragment>
+      <Navbar />
+
+      <main className='' >
+        <Container>
+
+        </Container>
+      </main>
+    </Fragment>
+  )
+}
+
+export default Home;
